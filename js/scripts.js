46070f096b8acc2712e13e31827ab06aafeadf3d
Brandon Madsen &  Kyle Smith <student@epicodus.com>
2014-07-23 14:06:48 -0700

    before list after refactor

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,15 @@ var Purchase = {
 $(document).ready(function(){
  $("form#expense-input").submit(function(event){
 
-    Purchase.description = $("input#desc").val();
-    Purchase.price = $("input#price").val();
-    Purchase.quantity = $("input#quant").val();
+    var newPurchase = Object.create(Purchase);
+    newPurchase.description = $("input#desc").val();
+    newPurchase.price = $("input#price").val();
+    newPurchase.quantity = $("input#quant").val();
 
 $("table#table-value").append("<tr><td>" + $("input#desc").val() + "</td>"
                       + "<td>$" + $("input#price").val() + "</td>" +
                        "<td>" + $("input#quant").val() + "</td>" + "<td>$" +
-                       Purchase.total() + "</td></tr>");
+                       newPurchase.total() + "</td></tr>");
 
 $("#table").show();
 event.preventDefault();
