d280dd871195749cd5a31e18bdc5f29dfd3e7327
Brandon Madsen &  Kyle Smith <student@epicodus.com>
2014-07-23 11:19:54 -0700

    2nd commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
-$("form#expense-input").submit(function(event){
-  $("input#desc").val();
-  $("input#price").val();
-  $("input#quant").val();
+ $("form#expense-input").submit(function(event){
 
-  ß
+    var pDescription = $("input#desc").val();
+    var pPrice = $("input#price").val();
+    var pQuantity = $("input#quant").val();
 
-})
-})
+event.preventDefault();
+
+
+$("table#table-value").append("<tr><td>" + $("input#desc").val() + "</td>"
+                      + "<td>" + $("input#price").val() + "</td>" +
+                       "<td>" + $("input#quant").val() "</td></tr>");
+
+
+$("input#desc").val("");
+$("input#price").val("");
+
+  });
+});
